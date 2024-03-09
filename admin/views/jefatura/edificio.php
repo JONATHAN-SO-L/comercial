10fbcc16271a03cd55b51b6f1e3ca215de74b01a
JONATHAN-SO-L <sanceznarval00@outlook.com>
2024-03-08 18:16:46 -0600

    Bug Fix 003: Redireccionamiento del botón 'Volver' para la pantalla de inicio

--- admin/views/jefatura/edificio.php
+++ admin/views/jefatura/edificio.php
@@ -1,7 +1,6 @@
 <?php session_start(); 
 if ($_SESSION['nombre'] && $_SESSION['tipo']) {
 	require '../../../config/conex.php';
-	$vendedor = $_SESSION['nombre'];
 	?>
 
 	<!DOCTYPE html>
@@ -12,7 +11,7 @@ if ($_SESSION['nombre'] && $_SESSION['tipo']) {
 	</head>
 	<body><br><br>
 		<div class="container col-sm-8 panel panel-body"><br><br>
-			<a href="../vendedor/index.php" class="btn btn-primary volver"><img class="volver_pic" src="../../../assets/img/volver.png"><strong style="margin-left: 5px;">Volver al inicio</strong></a>
+			<a href="../jefatura/index.php" class="btn btn-primary volver"><img class="volver_pic" src="../../../assets/img/volver.png"><strong style="margin-left: 5px;">Volver al inicio</strong></a>
 			<img class="empresa_pic" src="../../../assets/img/edificio.png" style="margin-right: 15px;"><h3 style="margin-top: 10px;">Agregar Edificio Nuevo</h3><br>
 			<div class="card">
 				<div class="card bg-primary text-white"><center><h6><strong>Para poder registrar un edificio nuevo debes de llenar todos los campos de este formulario</strong></h6></center></div>
--- admin/views/jefatura/empresa.php
+++ admin/views/jefatura/empresa.php
@@ -9,7 +9,7 @@ if ($_SESSION['nombre'] && $_SESSION['tipo']) {
 	</head>
 	<body><br><br>
 		<div class="container col-sm-8 panel panel-body"><br><br>
-			<a href="../vendedor/index.php" class="btn btn-primary volver"><img class="volver_pic" src="../../../assets/img/volver.png"><strong style="margin-left: 5px;">Volver al inicio</strong></a>
+			<a href="../jefatura/index.php" class="btn btn-primary volver"><img class="volver_pic" src="../../../assets/img/volver.png"><strong style="margin-left: 5px;">Volver al inicio</strong></a>
 			<img class="empresa_pic" src="../../../assets/img/empresa.png" style="margin-right: 15px;"><h3 style="margin-top: 10px;">Agregar Empresa Nueva</h3><br>
 			<div class="card">
 				<div class="card bg-primary text-white"><center><h6><strong>Para poder registrar una empresa nueva debes de llenar todos los campos de este formulario</strong></h6></center></div>
